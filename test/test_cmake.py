d8e3e4f6a89f238dc5c5a3279b267e3bdf784405
yerejm <github@thelostyerejm.com>
2015-12-09 21:34:57 +1000

    Extract cmake specific behaviour

---
+++ test/test_cmake.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_cmake
+----------------------------------
+
+Tests for `cmake` module.
+"""
+
+import os
+import platform
+import re
+import pytest
+
+from ttt import cmake
+from ttt.cmake import CMakeContext
+from ttt.cmake import CMakeError, InvalidCMakeGenerator
+from testfixtures import TempDirectory
+
+def is_windows():
+    return platform.system() == 'Windows'
+
+class TestCreateBuildArea:
+    def setup(self):
+        work_directory = TempDirectory()
+        work_directory.write(('source', 'CMakeLists.txt'), b'project(test)')
+        self.old_cwd = os.getcwd()
+        os.chdir(work_directory.path)
+
+    def teardown(self):
+        os.chdir(self.old_cwd)
+        TempDirectory.cleanup_all()
+
+    def test_build_file_requires_build(self):
+        ctx = CMakeContext('source')
+        assert ctx.build_file() is None
+        ctx.build()
+        assert ctx.build_file() is not None
+
+    def test_cmake_error_captures_output(self):
+        source_path = '{}'.format(os.path.join(os.getcwd(), 'dummy'))
+
+        ctx = CMakeContext('dummy')
+        try:
+            ctx.build()
+        except CMakeError as e:
+            assert e.command == [
+                'cmake',
+                '-G', 'Unix Makefiles',
+                '-H{}'.format(source_path),
+                '-B{}'.format(os.path.join(os.getcwd(), 'dummy-build'))
+            ]
+
+            error = 'The source directory "{}" does not exist.'.format(source_path)
+            assert error in e.message
+
+    def test_create_build_area_in_cwd(self):
+        ctx = CMakeContext('source')
+        ctx.build()
+
+        assert os.path.exists(os.path.join(os.getcwd(), 'source'))
+        assert os.path.exists(os.path.join(os.getcwd(), 'source-build', 'CMakeFiles'))
+
+    def test_create_existing_build_area(self):
+        ctx = CMakeContext('source')
+        ctx.build()
+        create_time = os.path.getctime(os.path.join(os.getcwd(), 'source-build', 'CMakeFiles'))
+        ctx.build()
+        recreate_time = os.path.getctime(os.path.join(os.getcwd(), 'source-build', 'CMakeFiles'))
+
+        assert create_time == recreate_time
+
+class TestDefaultBuildArea:
+
+    def teardown(self):
+        TempDirectory.cleanup_all()
+
+    def test_default_build(self):
+        cmake_source_directory = TempDirectory()
+        cmake_source_path = cmake_source_directory.path
+        cmake_build_directory = TempDirectory()
+        cmake_build_path = cmake_build_directory.path
+        cmake_source_directory.write('CMakeLists.txt', b'project(test)')
+
+        ctx = CMakeContext(cmake_source_path, cmake_build_path)
+        ctx.build()
+
+        default_build_command = 'msbuild' if is_windows() else 'make'
+        default_build_file_pattern = 'test.sln' if is_windows() else 'Makefile'
+
+        assert ctx.build_command() == default_build_command
+        assert ctx.build_file() == default_build_file_pattern
+        assert os.path.exists(os.path.join(ctx.build_path, 'CMakeFiles'))
+        assert os.path.exists(os.path.join(ctx.build_path, 'Makefile'))
+        assert ctx.watch_path == cmake_source_path
+        assert ctx.build_path == cmake_build_path
+
+class TestNinjaBuildArea:
+
+    def teardown(self):
+        TempDirectory.cleanup_all()
+
+    def test_ninja_build(self):
+        cmake_source_directory = TempDirectory()
+        cmake_source_path = cmake_source_directory.path
+        cmake_build_directory = TempDirectory()
+        cmake_build_path = cmake_build_directory.path
+        cmake_source_directory.write('CMakeLists.txt', b'project(test)')
+
+        ctx = CMakeContext(cmake_source_path, cmake_build_path, 'Ninja')
+        ctx.build()
+
+        assert ctx.build_command() == 'ninja'
+        assert ctx.build_file() == 'build.ninja'
+        assert os.path.exists(os.path.join(ctx.build_path, 'CMakeFiles'))
+        assert os.path.exists(os.path.join(ctx.build_path, 'build.ninja'))
+        assert ctx.watch_path == cmake_source_path
+        assert ctx.build_path == cmake_build_path
+
+class TestMakeBuildArea:
+
+    def teardown(self):
+        TempDirectory.cleanup_all()
+
+    def test_make_build(self):
+        cmake_source_directory = TempDirectory()
+        cmake_source_path = cmake_source_directory.path
+        cmake_build_directory = TempDirectory()
+        cmake_build_path = cmake_build_directory.path
+        cmake_source_directory.write('CMakeLists.txt', b'project(test)')
+
+        ctx = CMakeContext(cmake_source_path, cmake_build_path, 'Unix Makefiles')
+        ctx.build()
+
+        assert ctx.build_command() == 'make'
+        assert ctx.build_file() == 'Makefile'
+        assert os.path.exists(os.path.join(ctx.build_path, 'CMakeFiles'))
+        assert os.path.exists(os.path.join(ctx.build_path, 'Makefile'))
+        assert ctx.watch_path == cmake_source_path
+        assert ctx.build_path == cmake_build_path
+
+class TestInvalidBuildArea:
+
+    def teardown(self):
+        TempDirectory.cleanup_all()
+
+    def test_bad_build(self):
+        cmake_source_directory = TempDirectory()
+        cmake_source_path = cmake_source_directory.path
+        cmake_build_directory = TempDirectory()
+        cmake_build_path = cmake_build_directory.path
+        cmake_source_directory.write('CMakeLists.txt', b'project(test)')
+
+        with pytest.raises(InvalidCMakeGenerator):
+            CMakeContext(cmake_source_path, cmake_build_path, 'dummy')
+        assert not os.path.exists(os.path.join(cmake_build_path, 'CMakeFiles'))
+
---
+++ ttt/cmake.py
@@ -0,0 +1,136 @@
+import os
+import subprocess
+import multiprocessing
+import platform
+import glob
+import collections
+
+BuildSystem = collections.namedtuple('BuildSystem', [
+    'file',
+    'command',
+    'parallel_opt',
+    'file_opt',
+])
+BUILD_SYSTEMS = {
+    'Unix Makefiles': BuildSystem(
+        file='Makefile',
+        command='make',
+        parallel_opt='-j{}',
+        file_opt='-f{}'
+    ),
+    'Ninja': BuildSystem(
+        file='build.ninja',
+        command='ninja',
+        parallel_opt='-j{}',
+        file_opt='-f{}'
+    ),
+    'Visual Studio 14 2015': BuildSystem(
+        file='*.sln',
+        command='msbuild',
+        parallel_opt='/m:{}',
+        file_opt='{}'
+    ),
+}
+DEFAULT_BUILD_WINDOWS = 'Visual Studio 14 2015'
+DEFAULT_BUILD_UNIX = 'Unix Makefiles'
+DEFAULT_BUILD_PATH_SUFFIX = '-build'
+
+def default_build_system():
+    default = DEFAULT_BUILD_UNIX
+    if platform.system() is 'Windows':
+        DEFAULT_BUILD_WINDOWS
+    return default
+
+def make_build_path(watch_path, suffix=DEFAULT_BUILD_PATH_SUFFIX):
+    return os.path.join(
+        os.getcwd(),
+        "{}{}".format(os.path.basename(watch_path), suffix)
+    )
+
+class CMakeContext(object):
+    """
+    Provides a context in which cmake operations occur.
+    This will create a cmake build area for a source area if none exists and
+    provide a means to call the build command in that area.
+    """
+
+    def __init__(self, watch_path, build_path=None, build_system=default_build_system()):
+        if build_system not in BUILD_SYSTEMS:
+            raise InvalidCMakeGenerator()
+        self.build_system = build_system
+        self.watch_path = os.path.abspath(watch_path)
+        self.build_path = make_build_path(watch_path) if build_path is None else build_path
+        self.build_system = build_system
+
+    def build_file(self):
+        """ Get the build file for the cmake build area, e.g. Makefile."""
+        # Cache the build file to avoid repeating path traversal.
+        try:
+            return self._build_file
+        except AttributeError:
+            self._build_file = self._find_build_file()
+            return self._build_file
+
+    def build_command(self):
+        """ Get the build command for the cmake build area, e.g. make."""
+        return BUILD_SYSTEMS[self.build_system].command
+
+    def build(self):
+        """
+        Calls the build command in the build area. If no build area exists, it
+        will be created.
+        """
+        if not os.path.exists(os.path.join(self.build_path, 'CMakeFiles')):
+            self._cmake_generate()
+            self._build_file = self._find_build_file()
+        self._build()
+
+    def _build(self):
+        build = BUILD_SYSTEMS[self.build_system]
+        old_path = os.getcwd()
+        try:
+            os.chdir(self.build_path)
+            self._execute([
+                build.command,
+                build.parallel_opt.format(multiprocessing.cpu_count() + 2),
+                build.file_opt.format(self._build_file)
+            ])
+        finally:
+            os.chdir(old_path)
+
+    def _find_build_file(self):
+        build_file_pattern = BUILD_SYSTEMS[self.build_system].file
+        search_path = os.path.join(self.build_path, build_file_pattern)
+        print("Search for {}".format(search_path))
+        matches = glob.glob(search_path)
+        return os.path.basename(matches[0]) if matches else None
+
+    def _cmake_generate(self):
+        self._execute([
+            'cmake',
+            '-G', self.build_system,
+            '-H{}'.format(self.watch_path),
+            '-B{}'.format(self.build_path)
+        ])
+
+    def _execute(self, command):
+        try:
+            print(subprocess.check_output(command, stderr=subprocess.STDOUT, universal_newlines=True))
+        except subprocess.CalledProcessError as e:
+            raise CMakeError(command, e.output)
+
+class CMakeError(Exception):
+    """ Exception raised when a cmake command fails."""
+
+    def __init__(self, command, message):
+        self.command = command
+        self.message = message
+
+    def __str__(self):
+        return '{}\n{}'.format(repr(self.command), self.message)
+
+class InvalidCMakeGenerator(Exception):
+    """ Exception raised for a generator that is invalid on the current
+    platform."""
+    pass
+
