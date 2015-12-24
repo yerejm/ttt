47874e1c69cb106d94f6dc40f2f342db5ca7b57b
yerejm <github@thelostyerejm.com>
2015-12-24 22:46:40 +1000

    Remove version suffix from py.test command

--- test.sh
+++ test.sh
@@ -2,7 +2,4 @@
 export PYENV_VERSION=$@
 version=$(echo ${PYENV_VERSION} | cut -c 1-3)
 pytest=py.test
-if [ "${version}" != "3.5" ]; then
-    pytest=py.test-2.7
-fi
 ${pytest} -rw --lf -f --color=yes --cov ttt --cov-report term-missing test
