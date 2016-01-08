5326a0c414b2850e2208b29bcc35a45c449fa977
yerejm <github@thelostyerejm.com>
2016-01-08 22:15:35 +1000

    Reinstate the entry point expected by setup.py

--- ttt/__main__.py
+++ ttt/__main__.py
@@ -3,9 +3,12 @@ import colorama
 
 from ttt import cli
 
-if __name__ == "__main__":
+def main():
     if sys.stdout.isatty():
         colorama.init()
     cli.run()
     sys.exit(0)
+
+if __name__ == "__main__":
+    main()
 
