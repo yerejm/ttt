2b949a1a9bd55f4459c048c10921c2358fcb4145
yerejm <github@thelostyerejm.com>
2016-06-30 18:34:27 +1000

    update version

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
     license="ISCL",
     keywords='ttt',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: ISC License (ISCL)',
         'Natural Language :: English',
