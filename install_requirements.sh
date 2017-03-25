eebfa3aae0652dc8d07b6e50d1d60196149658e2
yerejm <github@thelostyerejm.com>
2017-03-25 11:55:33 +1000

    add automatic setup for available python versions

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
-PYENV_VERSION=3.6.0 pip install -r requirements.txt
-PYENV_VERSION=3.6.0 pip install -r dev-requirements.txt
-
-PYENV_VERSION=3.5.2 pip install -r requirements.txt
-PYENV_VERSION=3.5.2 pip install -r dev-requirements.txt
-
-PYENV_VERSION=2.7.13 pip install -r requirements-py27.txt
-PYENV_VERSION=2.7.13 pip install -r dev-requirements-py27.txt
+set -o pipefail
+set -o errexit
+pyenv versions | grep -v system | cut -c 3- | awk '{print $1}' > .python-version
+for pyver in `pyenv versions | grep python-version | awk '{print $2}'`; do
+    major_ver=`echo ${pyver} | awk -F. '{print $1}'`
+    export PYENV_VERSION=${pyver}
+    echo "Installing pip packages for python ${major_ver} (${pyver})"
+    case ${major_ver} in
+        3)
+            pip install -r requirements.txt && pip install -r dev-requirements.txt
+            ;;
+        2)
+            pip install -r requirements-py27.txt && pip install -r dev-requirements-py27.txt
+            ;;
+        *)
+            echo "Unknown python ${major_ver}!"
+            ;;
+    esac
+done
 
 pyenv rehash
