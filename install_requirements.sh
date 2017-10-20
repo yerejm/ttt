c04bb0f8ff59ea240580f1eab05b7bad575bdac2
yerejm <github@thelostyerejm.com>
2017-10-20 22:09:25 +1000

    test against installed python

--- install_requirements.sh
+++ install_requirements.sh
@@ -2,7 +2,7 @@
 set -o pipefail
 set -o errexit
 pyenv versions | grep -v system | cut -c 3- | awk '{print $1}' > .python-version
-for pyver in `pyenv versions | grep python-version | awk '{print $2}'`; do
+for pyver in $(pyenv versions | grep -v system | cut -c 3- | awk '{print $1}'); do
     major_ver=`echo ${pyver} | awk -F. '{print $1}'`
     export PYENV_VERSION=${pyver}
     echo "Installing pip packages for python ${major_ver} (${pyver})"
