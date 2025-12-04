62fac772b7522598755e573b23a23d5deb8e079a
yerejm <yerejm@gmail.com>
2025-12-05 08:55:36 +1000

    handle python upgrades

--- tool_version.sh
+++ tool_version.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 set -eux -o pipefail
-echo "python $(mise ls python | awk '{ver=(NR==1?ver:ver " ")$2}END{print ver}')" > .tool-versions
+echo "python $(mise ls python | grep -v missing | awk '{ver=(NR==1?ver:ver " ")$2}END{print ver}')" > .tool-versions
+uv tool install nox
